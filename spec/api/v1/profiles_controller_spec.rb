80153f4a9395a0ec480483504bd51742e3339914
Amanda Beiner <amandabeiner@Amandas-MacBook-Pro.local>
2017-10-21 16:14:02 -0400

    Profile create endpoint

--- spec/api/v1/profiles_controller_spec.rb
+++ spec/api/v1/profiles_controller_spec.rb
@@ -2,6 +2,23 @@ require "rails_helper"
 
 describe Api::V1::ProfilesController, type: :controller do 
   let(:json_parsed_response) { JSON.parse(response.body) } 
+  let(:correct_profile_params) {
+    { first_name: "Nichole", 
+      last_name: "Beiner", 
+      email: "nichole@bu.edu", 
+      role: "alum", 
+      contact_permissions: "public"
+    }
+  }
+
+  let(:incorrect_profile_params) {
+     { first_name: "Nichole", 
+      last_name: "Beiner", 
+      email: "nichole@notbu.edu", 
+      role: "alum", 
+      contact_permissions: "public"
+    }
+  }
   describe "GET #show" do
     let!(:profile) { FactoryGirl.create(:profile) }
 
@@ -12,6 +29,29 @@ describe Api::V1::ProfilesController, type: :controller do
       expect(response.content_type).to eq("application/json")
       expect(json_parsed_response.keys).to eq ["id", "first_name", "last_name", "email", "role", "contact_permissions", "phone", "position", "company"]
       expect(json_parsed_response["id"]).to eq profile.id  
+    end
+  end
+
+  describe "POST #create" do
+    it "creates a new profile" do
+      expect { post :create, params: { profile: correct_profile_params } }.to change { Profile.count }.by 1
+    end
+
+    it "returns the profile as JSON when the params are correct" do
+      post :create, params: { profile: correct_profile_params }
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+
+      expect(json_parsed_response.keys).to eq ["id", "first_name", "last_name", "email", "role", "contact_permissions", "phone", "position", "company"]
+    end
+
+    it "returns an error when the payload is incorrect" do
+      post :create, params: { profile: incorrect_profile_params }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json_parsed_response.keys).to eq ["error"]
+      expect(json_parsed_response["error"]).to eq({"email"=> ["You must have an active BU email address to register."]})
     end
   end
 end
